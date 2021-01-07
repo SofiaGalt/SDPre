633641f8e40f7b840f90fb28ecd4cc3e945424ca
Sofia Galt Kendall <sgkartist@gmail.com>
2021-01-07 13:24:24 -0700

    Objective 2

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,8 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String myVariable = "David";
+    System.out.println(myVariable);
+    myVariable = "Bowie";
+    System.out.println(myVariable);
+  }
+}
