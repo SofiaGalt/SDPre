440c5996ae2f692e8f61ad37d1941fc54fe24fa8
Sofia Galt Kendall <sgkartist@gmail.com>
2021-01-07 15:17:33 -0700

    Objective 3

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,12 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 57.389;
+    num2 = 34.689;
+    num3 = 88.2222482;
+
+    average = (num1 + num2 + num3) / 3;
+    System.out.println(average);
+  }
+}
