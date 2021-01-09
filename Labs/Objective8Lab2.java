e19d275245d56ac69d90ce9cf48e5dcc117374d1
Sofia Galt Kendall <sgkartist@gmail.com>
2021-01-09 09:19:04 -0700

    Objective 8.

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,12 @@
+public class Objective8Lab2 {
+  public static void main(String[] args){
+
+    int sum = 0;
+
+    for (int toAdd = 1 ; toAdd <= 20 ; toAdd++){
+      sum += toAdd;
+    }
+
+    System.out.println(sum);
+  }
+}
